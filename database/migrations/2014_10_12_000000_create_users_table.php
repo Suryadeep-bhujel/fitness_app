ebc5b5e67a908cc875d7942a9e7d4b32ed6a9dbb
Suryadeep Bhujel <srdpbhujel@gmail.com>
2023-03-12 14:10:19 +0530

    user migration updated , otplist model and migration created and updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('firstName', 40)->nullable();
+                $table->string('middleName', 40)->nullable();
+                $table->string('lastName', 40)->nullable();
+                $table->string('profilPhoto', 255)->nullable();
+                $table->string('email')->unique();
+                $table->string('mobile', 18)->unique();
+                $table->timestamp("dateOfBirth")->nullable();
+                $table->enum("gender", ['Male', "Female", "Other"])->nullable();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->timestamp('mobile_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
